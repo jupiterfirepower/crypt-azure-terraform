fa02693cc259e9dc4aed9ad5c982e0f87473d7c5
Oleh Parshenko <37039347+jupiterfirepower@users.noreply.github.com>
2024-04-16 10:19:30 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -50,7 +50,6 @@ terraform {
 }
 
 provider "azurerm" {
-#    features {}
     features {
       resource_group {
         prevent_deletion_if_contains_resources = false
@@ -61,8 +60,3 @@ provider "azurerm" {
 provider "azuread" {
   environment = "${environment}"
 }
-
-#provider "kubernetes" {
-#    config_path    = "~/.kube/config"
-#    config_context = "minikube"
-#}
